d864f69b676a8d8a219fa2d2d4164a4edeaa5932
ZhySir <916859568@qq.com>
2019-04-24 15:08:45 +0800

    OrderChart class

--- app/src/main/java/com/zhy/orderchart/MainActivity.java
+++ app/src/main/java/com/zhy/orderchart/MainActivity.java
@@ -1,13 +1,49 @@
 package com.zhy.orderchart;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.zhy.orderchart.view.OrderChart;
+
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    private OrderChart orderChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        initData();
     }
+
+    private void initView() {
+        orderChart = findViewById(R.id.orderChart);
+    }
+
+    private Handler handler = new Handler();
+    private int TIME = 500;
+    int tempMin = 50;
+    int tempMax = 90;
+    private Random random = new Random();
+
+    private void initData() {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                handler.postDelayed(this, TIME);
+
+                int tempTop = random.nextInt(tempMax) % (tempMax - tempMin + 1) + tempMin;
+                int tempDown = random.nextInt(tempMax) % (tempMax - tempMin + 1) + tempMin;
+
+                orderChart.setData(tempTop, tempDown);
+                orderChart.setDigits(1);
+            }
+        }, TIME);
+    }
+
 }
